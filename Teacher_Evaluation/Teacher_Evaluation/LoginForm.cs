41ac11243dffd0db1428cf8328aeb0d6d9e8b347
Alonzo-Jhon-Llyod-BSCS21A1 <C1335-23@itmlyceumalabang.onmicrosoft.com>
2024-10-30 19:48:09 +0800

    Add files via upload

---
+++ Teacher_Evaluation/Teacher_Evaluation/LoginForm.cs
@@ -0,0 +1,35 @@
+namespace Teacher_Evaluation
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
