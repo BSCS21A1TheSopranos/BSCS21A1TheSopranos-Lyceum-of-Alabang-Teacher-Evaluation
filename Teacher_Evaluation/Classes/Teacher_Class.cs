a993d591b71c77a2ba66d5bd5d59be508aad9365
Jhune Rey E. Magdurulan <c2014-23@itmlyceumalabang.onmicrosoft.com>
2024-10-31 15:33:35 +0800

    Teachers Class

---
+++ Teacher_Evaluation/Classes/Teacher_Class.cs
@@ -0,0 +1,33 @@
+﻿namespace Classes
+{
+    public class Teacher_Class
+    {
+        private int teachers_ID;
+        private string teachers_Name;
+        private List<string> teachers_Subject;
+
+        public Teacher_Class(int teachers_ID, string teachers_Name)
+        {
+            this.teachers_ID = teachers_ID;
+            this.teachers_Name = teachers_Name;
+        }
+
+        public int Teachers_ID
+        {
+            get { return teachers_ID;}
+            set { teachers_ID = value;}
+        }
+        public string Teachers_Name
+        {
+            get { return teachers_Name; }
+            set { teachers_Name = value; }
+        }
+
+        public void AddSubject(string subject) 
+        {
+            teachers_Subject.Add(subject);
+        }
+
+    }
+
+}
