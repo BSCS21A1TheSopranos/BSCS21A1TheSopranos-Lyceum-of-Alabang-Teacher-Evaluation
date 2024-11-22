97a0a7f81256dd81a9d71467cfa831998b44e79f
Marasigan-Paul-Foster-BSCS21A1 <c1115-23@itmlyceumalabang.onmicrosoft.com>
2024-11-22 20:45:28 +0800

    Updated Login Class

--- Teacher_Evaluation/ClassLibrary1/LoginClass.cs
+++ Teacher_Evaluation/ClassLibrary1/LoginClass.cs
@@ -9,12 +9,12 @@ namespace ClassLibrary1
 {
     public class LoginClass
     {
-   
-        public string studentID;
 
-        public LoginClass(string studentID)
+        public string id;
+
+        public LoginClass(string id)
         {
-            this.studentID = studentID;
+            this.id = id;
         }
 
         public bool checkpassword(string password)
@@ -25,19 +25,42 @@ namespace ClassLibrary1
                 byte[] data = md5.ComputeHash(utf8.GetBytes(password));
                 password = Convert.ToBase64String(data);
             }
-            return StudentDataHolder.Students[studentID].Password == password;
+
+            if (StudentDataHolder.Students.ContainsKey(id))
+                return StudentDataHolder.Students[id].Password == password;
+
+            if (TeacherDataHolder.Teachers.ContainsKey(id))
+                return TeacherDataHolder.Teachers[id].Password == password;
+
+            if (AdminDataHolder.Admins.ContainsKey(id))
+                return AdminDataHolder.Admins[id].Password == password;
+
+            return false;
         }
 
         public bool checkStudent()
         {
-            return StudentDataHolder.Students.ContainsKey(studentID);
+            return StudentDataHolder.Students.ContainsKey(id)
+                || TeacherDataHolder.Teachers.ContainsKey(id)
+                || AdminDataHolder.Admins.ContainsKey(id);
         }
 
         public string GetRole()
         {
-            return StudentDataHolder.Students[studentID].Role;
+            if (StudentDataHolder.Students.ContainsKey(id))
+                return StudentDataHolder.Students[id].Role;
+
+            if (TeacherDataHolder.Teachers.ContainsKey(id))
+                return TeacherDataHolder.Teachers[id].Role;
+
+            if (AdminDataHolder.Admins.ContainsKey(id))
+                return AdminDataHolder.Admins[id].Role;
+
+            return "Wala Boi";
         }
+
 
     }
 
 }
+this.id
