d05de085af33dac2a6c055bef1644b44a4d4dffa
Zozobrado-LeoMarc-BSCS21A1 <c1828-23@itmlyceumalabang.onmicrosoft.com>
2024-12-03 22:10:20 +0800

    Add files via upload

---
+++ UI_RegisterStud.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace Teacher_Evaluation
+{
+    partial class UI_RegisterStud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TextBox textBox1;
+            button1 = new Button();
+            label1 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label2 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            checkBox1 = new CheckBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.DimGray;
+            textBox1.Location = new Point(492, 121);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Enter full name";
+            textBox1.Size = new Size(281, 24);
+            textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(243, 196, 27);
+            button1.FlatAppearance.BorderColor = Color.Gold;
+            button1.FlatAppearance.BorderSize = 5;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(517, 324);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 31);
+            button1.TabIndex = 0;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(248, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 25);
+            label1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Microsoft Sans Serif", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(36, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(197, 55);
+            label3.TabIndex = 3;
+            label3.Text = "Student";
+            // 
+            // textBox2
+            // 
+            textBox2.AccessibleDescription = "";
+            textBox2.AccessibleName = "";
+            textBox2.BackColor = Color.White;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.ForeColor = Color.DimGray;
+            textBox2.Location = new Point(492, 225);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Create new password";
+            textBox2.Size = new Size(281, 24);
+            textBox2.TabIndex = 6;
+            textBox2.Tag = "";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.White;
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.ForeColor = Color.DimGray;
+            textBox3.Location = new Point(492, 169);
+            textBox3.Margin = new Padding(3, 2, 3, 2);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Enter Student ID";
+            textBox3.Size = new Size(281, 24);
+            textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Microsoft Sans Serif", 36.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(36, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(143, 57);
+            label4.TabIndex = 4;
+            label4.Text = "Form";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft Sans Serif", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(36, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(292, 55);
+            label2.TabIndex = 2;
+            label2.Text = "Registration";
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.FromArgb(31, 51, 92);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(415, -35);
+            label5.Name = "label5";
+            label5.Size = new Size(460, 556);
+            label5.TabIndex = 9;
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.BackColor = SystemColors.ScrollBar;
+            label6.Location = new Point(457, 52);
+            label6.Name = "label6";
+            label6.Size = new Size(350, 395);
+            label6.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.BackColor = Color.FromArgb(189, 189, 189);
+            label7.Location = new Point(457, 52);
+            label7.Name = "label7";
+            label7.Size = new Size(350, 388);
+            label7.TabIndex = 11;
+            // 
+            // label8
+            // 
+            label8.BorderStyle = BorderStyle.FixedSingle;
+            label8.Location = new Point(457, 40);
+            label8.Name = "label8";
+            label8.Size = new Size(350, 389);
+            label8.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.BackColor = SystemColors.ControlLightLight;
+            label9.BorderStyle = BorderStyle.FixedSingle;
+            label9.Location = new Point(457, 40);
+            label9.Name = "label9";
+            label9.Size = new Size(350, 55);
+            label9.TabIndex = 13;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(540, 52);
+            label10.Name = "label10";
+            label10.Size = new Size(213, 24);
+            label10.TabIndex = 14;
+            label10.Text = "Create a new account";
+            // 
+            // textBox4
+            // 
+            textBox4.AccessibleDescription = "";
+            textBox4.AccessibleName = "";
+            textBox4.BackColor = Color.White;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.ForeColor = Color.DimGray;
+            textBox4.Location = new Point(492, 276);
+            textBox4.Margin = new Padding(3, 2, 3, 2);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Confirm password";
+            textBox4.Size = new Size(281, 24);
+            textBox4.TabIndex = 16;
+            textBox4.Tag = "";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Control;
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.SteelBlue;
+            button2.Location = new Point(517, 372);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(220, 31);
+            button2.TabIndex = 17;
+            button2.Text = "Already have an account?";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.White;
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Font = new Font("Microsoft Sans Serif", 11.25F);
+            textBox5.ForeColor = Color.DimGray;
+            textBox5.Location = new Point(122, 124);
+            textBox5.Margin = new Padding(3, 2, 3, 2);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "Enter Name";
+            textBox5.Size = new Size(288, 24);
+            textBox5.TabIndex = 18;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.White;
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Font = new Font("Microsoft Sans Serif", 11.25F);
+            textBox6.ForeColor = Color.DimGray;
+            textBox6.Location = new Point(122, 169);
+            textBox6.Margin = new Padding(3, 2, 3, 2);
+            textBox6.Name = "textBox6";
+            textBox6.PlaceholderText = "Enter Name";
+            textBox6.Size = new Size(288, 24);
+            textBox6.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.White;
+            textBox7.BorderStyle = BorderStyle.FixedSingle;
+            textBox7.Font = new Font("Microsoft Sans Serif", 11.25F);
+            textBox7.ForeColor = Color.DimGray;
+            textBox7.Location = new Point(122, 225);
+            textBox7.Margin = new Padding(3, 2, 3, 2);
+            textBox7.Name = "textBox7";
+            textBox7.PlaceholderText = "Enter Name";
+            textBox7.Size = new Size(288, 24);
+            textBox7.TabIndex = 20;
+            // 
+            // textBox8
+            // 
+            textBox8.BackColor = Color.White;
+            textBox8.BorderStyle = BorderStyle.FixedSingle;
+            textBox8.Font = new Font("Microsoft Sans Serif", 11.25F);
+            textBox8.ForeColor = Color.DimGray;
+            textBox8.Location = new Point(122, 276);
+            textBox8.Margin = new Padding(3, 2, 3, 2);
+            textBox8.Name = "textBox8";
+            textBox8.PlaceholderText = "Enter Name";
+            textBox8.Size = new Size(288, 24);
+            textBox8.TabIndex = 21;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(758, 305);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 22;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // UI_RegisterStud
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.load_blurred;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(834, 472);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "UI_RegisterStud";
+            Text = "UI_RegisterStud";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox4;
+        private Button button2;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private CheckBox checkBox1;
+    }
+}
---
+++ UI_RegisterStud.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using ClassLibrary1;
+namespace Teacher_Evaluation
+{
+    public partial class UI_RegisterStud : Form
+    {
+        public UI_RegisterStud()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox6.Text, textBox7.Text, textBox8.Text, "Student");
+            RegistrationClass registration = new RegistrationClass();
+            LoginForm loginForm = new LoginForm();
+
+            if (!registration.validstudentNo(student))
+            {
+                textBox6.Text = "Invalid Format. It must be ####-##";
+            }
+            if (!registration.checkpassword(student)) {
+                textBox7.Text = "Wrong Password";
+            }
+            if (!registration.validpaassword(textBox8.Text))
+            {
+                textBox8.Text = "Minimum of 8 characters with at least one uppercase, lowercase, number, and special character (@, $, !, %, *, ?, &).";
+            }
+            if (registration.validstudentNo(student) && (registration.validpaassword(textBox8.Text)))
+            {
+                registration.encryptpassword(textBox8.Text, student);
+                RegistrationClass.Update(student);
+                loginForm.Show();
+                this.Close();
+            }
+
+        }
+
+        private void UI_RegisterStud_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
